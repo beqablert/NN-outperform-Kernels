a8d9ae9afe3c55c73343ba291b060d9fd58eefb4
beqablert <106820696+beqablert@users.noreply.github.com>
2022-06-12 17:53:04 +0200

    Add files via upload

--- linearized_neural_networks-master/Arda-paper1.py
+++ linearized_neural_networks-master/Arda-paper1.py
@@ -241,7 +241,7 @@ class NeuralNetwork(nn.Module):
     self.g=nn.ReLU()
     self.soft = nn.Softmax(dim=1)
     self.K=K
-    self.loss= 
+    self.loss = square_loss
     self.drop = nn.Dropout(p=p)
     #change bias to true
     #self.fc1 = nn.utils.weight_norm(nn.Linear(N, K, bias=False))
