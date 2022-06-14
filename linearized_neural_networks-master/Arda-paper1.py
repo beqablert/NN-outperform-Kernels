4329f0149f6645625f53f97adf821c57afc112e6
beqablert <106820696+beqablert@users.noreply.github.com>
2022-06-14 17:13:07 +0200

    Add files via upload

--- linearized_neural_networks-master/Arda-paper1.py
+++ linearized_neural_networks-master/Arda-paper1.py
@@ -81,7 +81,7 @@ def train(model, loss_fn, train_data, val_data, epochs=750, device='cpu',model_n
             # print(yhat)
             # print(y)
             loss = loss_fn(yhat, y)
-            loss = loss + nn.MSELoss(model.wReg)
+            loss = loss + nn.MSELoss(model.wReg)*1e-2
             # print(loss)
             loss.backward()
             optimizer.step()
