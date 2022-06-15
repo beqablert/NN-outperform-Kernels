7f3ffbd2f90d67d2b17cbfecddd319709abd06c2
beqablert <106820696+beqablert@users.noreply.github.com>
2022-06-15 15:55:56 +0200

    Add files via upload

--- linearized_neural_networks-master/Arda-paper1.py
+++ linearized_neural_networks-master/Arda-paper1.py
@@ -301,7 +301,7 @@ class RF_Network(nn.Module):
         #First layer weights are fixed!
         self.w = np.random.randn(256,K)
         norm = np.linalg.norm(self.w,axis=0,keepdims=True)
-        self.w = self.w/(norm*np.sqrt(K))
+        self.w = self.w/(norm*10)
         self.w = torch.from_numpy(self.w)
         self.w = self.w.float()
         self.w = self.w.cuda()
