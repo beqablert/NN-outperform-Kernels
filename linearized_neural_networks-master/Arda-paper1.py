62c6f4078f833a5afe9467d1945605866d74bd26
beqablert <106820696+beqablert@users.noreply.github.com>
2022-06-17 00:50:14 +0200

    Add files via upload

--- linearized_neural_networks-master/Arda-paper1.py
+++ linearized_neural_networks-master/Arda-paper1.py
@@ -123,14 +123,14 @@ def train(model, loss_fn, train_data, val_data, epochs=750, device='cpu',model_n
         val_acc  = num_val_correct / num_val_examples
         val_loss = val_loss / len(val_dl.dataset)        
 
-        if epoch == 1 or epoch % 100 == 0 or epoch == 750: #show progress every 10 epochs
+        if epoch == 1 or epoch % 10 == 0 or epoch == 750: #show progress every 10 epochs
           print('Epoch %3d/%3d, train loss: %5.2f, train acc: %5.2f, val loss: %5.2f, val acc: %5.2f' % \
                 (epoch, epochs, train_loss, train_acc, val_loss, val_acc))
           if epoch == 750:
             history['yhat_norm'].append(float((torch.linalg.norm(yhat, dim=0, ord=2) ** 2)/len(val_dl.dataset)))
             history['y_norm'].append(float((torch.linalg.norm(y, dim=0, ord=2) ** 2)/len(val_dl.dataset)))
-          print(yhat)
-          print(loss)  
+        #   print(yhat)
+        #   print(loss)  
           print((torch.linalg.norm(yhat, dim=0, ord=2) ** 2)/len(val_dl.dataset))
           print((torch.linalg.norm(y, dim=0, ord=2) ** 2)/len(val_dl.dataset))
           print((val_loss - (torch.linalg.norm(y, dim=0, ord=2) ** 2)/len(val_dl.dataset))/((torch.linalg.norm(yhat, dim=0, ord=2) ** 2)/len(val_dl.dataset)))      
